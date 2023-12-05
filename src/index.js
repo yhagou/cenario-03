5783c21f75f59701535e2207b7a21ad1baa25c58
yhagou <151045107+yhagou@users.noreply.github.com>
2023-12-05 19:12:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//importações necessárias para o projeto
+//importações necessárias para o projeto 
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
@@ -11,30 +11,110 @@ const cors = require("cors");
 app.use(bodyParser.json());
 app.use(cors());
 
-//rota que lista todos os usuários cadastrados
+// Rota que lista todos os usuários cadastrados
 app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+    res.status(500).json({ error: "Erro ao buscar usuários" });
+  }
 });
 
-//rota que cadastra um usuário
+// Rota que cadastra um usuário
 app.post("/user", async (req, res) => {
+  const { name, email, age } = req.body;
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        age,
+      },
+    });
+    res.json(newUser);
+  } catch (error) {
+    console.error("Erro ao cadastrar usuário:", error);
+    res.status(500).json({ error: "Erro ao cadastrar usuário" });
+  }
 });
 
-//rota que apaga um usuário, passando o id
+// Rota que apaga um usuário, passando o id
 app.delete("/user/:id", async (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  try {
+    const deletedUser = await prisma.user.delete({
+      where: {
+        id: userId,
+      },
+    });
+    res.json(deletedUser);
+  } catch (error) {
+    console.error("Erro ao apagar usuário:", error);
+    res.status(500).json({ error: "Erro ao apagar usuário" });
+  }
 });
 
-//rota que atualiza um usuário, pelo id
+// Rota que atualiza um usuário, pelo id
 app.put("/user/:id", async (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  const { name, email, age } = req.body;
+  try {
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        name,
+        email,
+        age,
+      },
+    });
+    res.json(updatedUser);
+  } catch (error) {
+    console.error("Erro ao atualizar usuário:", error);
+    res.status(500).json({ error: "Erro ao atualizar usuário" });
+  }
 });
 
-//rota que lista usuários que contenham o nome específico
+// Rota que lista usuários que contenham o nome específico
 app.get("/users/:name", async (req, res) => {
+  const userName = req.params.name;
+  try {
+    const users = await prisma.user.findMany({
+      where: {
+        name: {
+          contains: userName,
+        },
+      },
+    });
+    res.json(users);
+  } catch (error) {
+    console.error("Erro ao buscar usuários por nome:", error);
+    res.status(500).json({ error: "Erro ao buscar usuários por nome" });
+  }
 });
 
-//rota que lista um usuário pelo id
+// Rota que lista um usuário pelo id
 app.get("/user/:id", async (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) {
+      res.status(404).json({ error: "Usuário não encontrado" });
+      return;
+    }
+    res.json(user);
+  } catch (error) {
+    console.error("Erro ao buscar usuário por id:", error);
+    res.status(500).json({ error: "Erro ao buscar usuário por id" });
+  }
 });
-
 // Inicie o servidor na porta especificada
 const server = app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
